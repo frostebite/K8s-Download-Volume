276bf70a431ac65871febbb91703bfb47a2bfc79
Frostebite <jas.f.ukcmti@gmail.com>
2020-06-29 23:53:27 +0100

    manually install kubectl

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh -l
 
-apt-get update && apt-get install -y apt-transport-https gnupg2
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
-apt-get update
-apt-get install -y kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
 
-kubectl get ns
+kubectl version
 echo "Hello $1"
 time=$(date)
 echo ::set-output name=time::$time
