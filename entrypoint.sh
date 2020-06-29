c73af7754f17c456e93aebe03788677b916776e4
Frostebite <jas.f.ukcmti@gmail.com>
2020-06-29 23:15:42 +0100

    Install kubectl

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh -l
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
 kubectl get ns
 echo "Hello $1"
 time=$(date)
