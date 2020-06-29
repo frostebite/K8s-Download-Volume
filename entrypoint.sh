91fe3c74409139860ba4042e44e3f9d04e9cb46a
Frostebite <jas.f.ukcmti@gmail.com>
2020-06-30 00:15:48 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,8 @@
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mv ./kubectl /usr/local/bin/kubectl
 
-kubectl version
+./kubectl version
 echo "Hello $1"
 time=$(date)
 echo ::set-output name=time::$time
