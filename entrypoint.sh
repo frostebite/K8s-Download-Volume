7559e6e81061345c1a7a46e4df41c48fa3bdcce3
Frostebite <jas.f.ukcmti@gmail.com>
2020-07-08 19:10:20 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
-
+ls $GITHUB_WORKSPACE
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 
