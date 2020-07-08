e7db956fcbcbd73a8628326bcd4eb97305143a6a
Frostebite <jas.f.ukcmti@gmail.com>
2020-07-08 22:13:43 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 ls $GITHUB_WORKSPACE
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-whoami
-sudo cp ./kubeconfig /.kube/config
+cp ./kubeconfig /.kube/config
 ./kubectl version
 echo "Hello $1"
 time=$(date)
