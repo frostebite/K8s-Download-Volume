8fb52380ca3d1399f231a105f3543faee638bfb8
Frostebite <jas.f.ukcmti@gmail.com>
2020-07-08 23:40:02 +0100

    create ftp job test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,15 @@ kubectl version
 echo "Hello $1"
 time=$(date)
 echo ::set-output name=time::$time
+
+cat <<EOF | kubectl apply -f -
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: FTPJOB
+spec:
+  template:
+    containers:
+    - name: FTPServer
+      image: stilliard/pure-ftpd
+EOF
