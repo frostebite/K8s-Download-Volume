5fcab62247d7147f17967c92e03d3346ed5109ed
Frostebite <jas.f.ukcmti@gmail.com>
2020-07-09 00:43:27 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,6 +24,6 @@ spec:
 EOF
 kubectl wait --for=condition=ready pod -l job-name=ftpjob-$GITHUB_SHA --timeout=60s
 kubectl port-forward jobs/ftpjob-$GITHUB_SHA 21:21 & 
-lftp -u test -p test ftp://localhost
+lftp ftp://test:test@localhost
 lftp -c put kubeconfig
 lftp -c ls
