63ab76bc056689cb494abe9cbfa72ff9463deb71
Frostebite <jas.f.ukcmti@gmail.com>
2020-07-09 01:25:33 +0100

    lftp

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,12 +12,14 @@ spec:
       restartPolicy: Never
       containers:
       - name: ftpserver
-        image: atmoz/sftp
+        image: stilliard/pure-ftpd
         env:
-        - name: SFTP_USERS
-          value: test:test
+        - name: FTP_USER_NAME
+          value: test
+        - name: FTP_USER_PASS
+          value: test
 EOF
 kubectl wait --for=condition=ready pod -l job-name=ftpjob-$GITHUB_SHA --timeout=60s
 kubectl port-forward jobs/ftpjob-$GITHUB_SHA 22:22 & 
 sleep 5
-sftp test:test@127.0.0.1:22
+lftp ftp://test:test@127.0.0.1:22
