335e29b083a7d0cf6b18860d1bcad2638b538cf3
Frostebite <jas.f.ukcmti@gmail.com>
2020-07-09 14:38:26 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,6 +28,6 @@ kubectl exec jobs/ftpjob-$GITHUB_SHA -- ls /data/repo
 kubectl exec jobs/ftpjob-$GITHUB_SHA -- apt-get update
 kubectl exec jobs/ftpjob-$GITHUB_SHA -- apt-get install zip unzip
 kubectl exec jobs/ftpjob-$GITHUB_SHA -- zip -r /output.zip /data/$2
-kubectl cp /output.zip jobs/ftpjob-$GITHUB_SHA
+kubectl cp jobs/ftpjob-$GITHUB_SHA:/output.zip 
 ls
 kubectl delete jobs/ftpjob-$GITHUB_SHA
