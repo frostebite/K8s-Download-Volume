f23147d7485b98767e6828281ddd15d5943de35a
Frostebite <jas.f.ukcmti@gmail.com>
2020-07-24 20:56:48 +0100

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,7 +42,7 @@ kubectl wait --for=condition=ready pod -l job-name=$DOWNLOAD_NAME --timeout=$5s
 kubectl exec jobs/$DOWNLOAD_NAME -- ls /data/$3
 kubectl exec jobs/$DOWNLOAD_NAME -- apk update
 kubectl exec jobs/$DOWNLOAD_NAME -- apk add zip
-kubectl exec jobs/$DOWNLOAD_NAME -- zip -r -j /output.zip /data/$3
+kubectl exec jobs/$DOWNLOAD_NAME -- zip -r -j output.zip /data/$3
 kubectl exec jobs/$DOWNLOAD_NAME -- stat /output.zip
 pods=$(kubectl get pods --selector=job-name=$DOWNLOAD_NAME --output=jsonpath='{.items[*].metadata.name}')
 kubectl cp $pods:output.zip $PWD/output.zip
